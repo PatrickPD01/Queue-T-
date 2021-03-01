5bc4d5f95a98af6b8f03e0fd68b87ce2de28f521
PatrickPD01 <61233451+PatrickPD01@users.noreply.github.com>
2021-03-01 09:22:42 +0100

    Add files via upload

---
+++ Køen/Køen/People.cs
@@ -0,0 +1,5 @@
+﻿public class Guest
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ Køen/Køen/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Køen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //<Summary>
+            // Queues the guests
+            Queue<Guest> guests = new Queue<Guest>();
+            guests.Enqueue(new Guest() { Age = 25, Name = "John" });
+            guests.Enqueue(new Guest() { Age = 24, Name = "Barbara" });
+            guests.Enqueue(new Guest() { Age = 24, Name = "Phil" });
+            guests.Enqueue(new Guest() { Age = 23, Name = "Fred" });
+            guests.Enqueue(new Guest() { Age = 26, Name = "Hannah" });
+            guests.Enqueue(new Guest() { Age = 27, Name = "Cindy" });
+            Debug.WriteLine("Full guest list in the queue:");
+            
+           //<Summary>
+           //Prints the guest name and age to the console
+            foreach (Guest guest in guests)
+            {
+                Debug.WriteLine("{0}, {1}", guest.Name, guest.Age);
+            }
+            
+                Debug.WriteLine("");
+                Guest firstToLeave = guests.Dequeue();
+                Debug.WriteLine(string.Format("First to leave: {0}", firstToLeave.Name));
+                Debug.WriteLine("Guest list after the first goodbye:");
+
+            
+            foreach (Guest guest in guests)
+            {
+                Debug.WriteLine("{0}, {1}", guest.Name, guest.Age);
+            }
+                Guest soonToLeave = guests.Peek();
+                Debug.WriteLine(string.Format("Just checking who'll leave next: {0}",
+                soonToLeave.Name));
+
+        }
+
+    }
+}
